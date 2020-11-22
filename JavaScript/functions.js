1342d19e615a8f29ca6e5841f8a23a3ade6579f5
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-11-22 19:12:52 -0300

    Tela de Carrinho de Compras - GuitART
    Google Maps API
    Ajustes de margens entre ícones e textos

---
+++ JavaScript/functions.js
@@ -0,0 +1,16 @@
+
+function initMap() {
+  var configuracoes = {
+    center: {lat: 11.551536855884436, lng: 92.24415040434234},
+    zoom: 12
+  }
+
+  var mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
+
+  var marcador = new google.maps.Marker({
+    position: {lat: 11.551536855884436, lng: 92.24415040434234},
+    title: "Ilha Sentilena do Norte",
+    map: mapa
+  });
+}
+
